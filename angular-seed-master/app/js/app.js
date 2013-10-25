de98c4b68005b24d27fa5852b734e34bf9cdc3bf
mohanlate <mohan.late@gmail.com>
2013-10-25 06:13:36 +0530

    uses routes

--- angular-seed-master/app/js/app.js
+++ angular-seed-master/app/js/app.js
@@ -2,15 +2,12 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
+angular.module('toyBuddyApp', [
   'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
+  'toyBuddyApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/catalog', {templateUrl: 'partials/catalog.html', controller: 'ctrlCatalog'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/'});
 }]);
